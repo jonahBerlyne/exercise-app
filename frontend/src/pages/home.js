35dddfa1d0c2ed4461e2e8a551371029a29bacd9
jonahBerlyne <jonshaber@gmail.com>
2022-08-09 19:31:49 -0400

    added bearer token to frontend

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
@@ -7,10 +8,15 @@ import WorkoutForm from "../components/WorkoutForm";
 const Home = () => {
 
  const { workouts, dispatch } = useWorkoutsContext();
+ const { user } = useAuthContext();
 
  useEffect(() => {
   const fetchWorkouts = async () => {
-   const response = await fetch('/api/workouts');
+   const response = await fetch('/api/workouts', {
+    headers: {
+     'Authorization': `Bearer ${user.token}`
+    }
+   });
    const json = await response.json();
    if (response.ok) {
     dispatch({
@@ -19,8 +25,8 @@ const Home = () => {
     });
    }
   }
-  fetchWorkouts();
- }, [dispatch]);
+  if (user) fetchWorkouts();
+ }, [dispatch, user]);
 
  return (
   <div className="home">
