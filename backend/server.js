1b664c99d8c57e05b571184613421b93c623da92
jonahBerlyne <jonshaber@gmail.com>
2022-08-09 13:40:03 -0400

    added workout model

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -15,4 +15,8 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workoutRoutes);
 
-app.listen(process.env.PORT, () => console.log('listening on port', process.env.PORT));
+mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+         app.listen(process.env.PORT, () => console.log('connected to db & listening on port', process.env.PORT));
+        })
+        .catch(err => console.log(err));
